9ca0077ee92aaf6ee85037901535877dabafb165
Rex-Fh <ivan6469@qq.com>
2025-04-19 19:48:13 +0800

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+int main() 
+{
+/*     char arr1[]="123456";
+    char arr2[6]={'1','2','3','4','5','6'};
+
+
+    printf("%s\n",arr1); */
+/*     printf("%d\n",strlen("c:\test\628\test.c")); */
+    int arr[]={1,2,3,4,5,6,7,8};
+    printf("%d/n",arr[6]);
+  /*   int i=0;
+
+    while(i<8)
+    {
+        printf("%d ",arr[i]);
+        i++;
+    } */
+    return 0;
+}
